fc7a29e9e7a7cfe86269c7a31bda3f200af371aa
carlos <carlos.gonzalez@openmailbox.org>
2016-03-03 22:19:35 +0100

    envio de correos no da error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 NestaBookshare::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
+config.action_mailer.perform_deliveries = true
+
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
@@ -16,6 +18,9 @@ NestaBookshare::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.default_url_options = { :host => '0.0.0.0:3000' }
 
   # Print deprecation notices to the Rails logger
